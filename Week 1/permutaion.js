b1a4bcd29b3ccd3496e843f86659464c009ea113
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-23 17:18:01 +0000

    Jul 23, 2023, 6:18 PM

--- Week 1/permutaion.js
+++ Week 1/permutaion.js
@@ -4,6 +4,6 @@ Str = abc
 using an if statement to check if the value is in then it exists
 
 a way to change the order of each character occurence
-compare the current state of the 
+compare the current state of the check with
 */
 
