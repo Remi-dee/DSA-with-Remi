347bd42e4399f2e048a1bd0a7e98571823df6498
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-23 20:28:25 +0000

    Jul 23, 2023, 9:28 PM

---
+++ Week 1/recursion.js
@@ -0,0 +1,61 @@
+/**
+ *
+ * input = abc
+ *
+ * output = ['bac', 'cab', 'acb', 'bca', 'cba']
+ *
+ *
+ * base case = str.length <= 1 return [str]
+ *
+ * input = abc
+ *
+ * firstchar = str[0] // abc
+ * remainchar = recursion(str(1)) // bc
+ * ['bc', 'cb']
+ *
+ * for(remaining of remianingChar)
+ * for (let i = 0; i < remaining.length; i++)
+ * result.push(remaining.slice(0,i) + firstchar + remaining.slice(i))
+ *
+ *
+ * result.push(input)
+ *
+ * ['bac','cab', abc']
+ *
+ * input = bc
+ * firstchar = str[0]
+ * result = []
+ * remainchar = recursion(str.slice(1)) // c
+ * ['c']
+ *
+ * for(remaining of remianingChar)
+ * for (let i = 0; i < remaining.length; i++)
+ * result.push(remaining.slice(0,i) + firstchar + remaining.slice(i))
+ *
+ *
+ *
+ * return result
+ *
+ * input = c
+ * return ['c']
+ */
+
+function recursion(str) {
+    console.log(str.length, str);
+    if (str.length <= 1) {
+      return [str];
+    }
+  
+    let firstchar = str[0];
+    let remianingChar = recursion(str.slice(1));
+    let result = [];
+  
+    for (const remaining of remianingChar) {
+      for (let i = 0; i < remaining.length; i++) {
+        result.push(remaining.slice(0, i) + firstchar + remaining.slice(i));
+      }
+    }
+    return result;
+  }
+  
+  console.log(recursion("abc"));
