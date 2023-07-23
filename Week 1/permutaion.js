4c0c5142c23503b9b2435c9ca8d4960b38975570
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-23 17:19:27 +0000

    Jul 23, 2023, 6:19 PM

--- Week 1/permutaion.js
+++ Week 1/permutaion.js
@@ -7,6 +7,8 @@ a way to change the order of each character occurence
 compare the current state of the check with the occurence of next string
 
 we need to have an anchor 
+we need to have a starting point 
+having 
 
 */
 
