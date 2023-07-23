af159000dd74e8e619bada82408b5307df1f58fa
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-23 17:15:25 +0000

    Jul 23, 2023, 6:15 PM

--- Week 1/permutaion.js
+++ Week 1/permutaion.js
@@ -1,7 +1,7 @@
 /*Problem: Generate all possible permutations of a given string using recursion.
 Str = abc
 
-using an if statement to check if the value e
+using an if statement to check if the value is in then it exists
 
 */
 
