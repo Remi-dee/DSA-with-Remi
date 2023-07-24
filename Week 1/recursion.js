1dd3cc4047affe0a281175e69915af5a2a7a2cca
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-24 17:12:31 +0000

    Jul 24, 2023, 6:12 PM

--- Week 1/recursion.js
+++ Week 1/recursion.js
@@ -41,49 +41,46 @@
  */
 
 function recursion(str) {
-    console.log(str);
-    if (str.length <= 1) {
-      return [str];
-    }
-  
-    let firstchar = str[0];
-    console.log(firstchar)
-    let remainingChar = recursion(str.slice(1));
-    
-    
-    let result = [];
-  
-    for (const remaining of remainingChar) {
-      for (let i = 0; i <= remaining.length; i++) {
-        console.log(remaining.slice(0,i))
-        result.push(remaining.slice(0, i) + firstchar + remaining.slice(i));
-      }
-    }
-    return result;
+  if (str.length <= 1) {
+    return [str];
   }
-  
-  console.log(recursion("abc"));
 
+  let firstchar = str[0];
+  console.log(firstchar);
+  let remainingChar = recursion(str.slice(1));
 
-  // function recursion(str) {
-  //   if (str.length <= 1) {
-  //     return [str];
-  //   }
-  
-  //   let firstchar = str[0];
-  //   let remainingChar = recursion(str.slice(1));
-  
-  //   let result = [];
-  
-  //   for (const remaining of remainingChar) {
-  //     for (let i = 0; i <= remaining.length; i++) { // Start from i = 0 and include the end of the string.
-  //       result.push(remaining.slice(0, i) + firstchar + remaining.slice(i));
-  //     }
-  //   }
-  
-  //   return result;
-  // }
-  
-  // const inputString = "abc";
-  // const result = recursion(inputString);
-  // console.log(result);
+  let result = [];
+
+  for (const remaining of remainingChar) {
+    for (let i = 0; i <= remaining.length; i++) {
+      console.log(remaining.slice(0, i));
+      result.push(remaining.slice(0, i) + firstchar + remaining.slice(i));
+    }
+  }
+  return result;
+}
+
+console.log(recursion("abc"));
+
+// function recursion(str) {
+//   if (str.length <= 1) {
+//     return [str];
+//   }
+
+//   let firstchar = str[0];
+//   let remainingChar = recursion(str.slice(1));
+
+//   let result = [];
+
+//   for (const remaining of remainingChar) {
+//     for (let i = 0; i <= remaining.length; i++) { // Start from i = 0 and include the end of the string.
+//       result.push(remaining.slice(0, i) + firstchar + remaining.slice(i));
+//     }
+//   }
+
+//   return result;
+// }
+
+// const inputString = "abc";
+// const result = recursion(inputString);
+// console.log(result);
