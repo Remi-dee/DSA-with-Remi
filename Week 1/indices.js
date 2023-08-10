8fbf484dc24f7edd57ef00a5ea376e963636ac49
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-10 18:21:50 +0000

    Thursday, August 10, 2023 at 7:21 PM West Africa Standard Time

--- Week 1/indices.js
+++ Week 1/indices.js
@@ -2,14 +2,15 @@ function indices(nums, target) {
   let output = [];
   for (let i = 0; i < nums.length; i++) {
     console.log(nums.length);
-
-    if (nums[i] + nums[i + 1] === target) {
-      console.log(i);
-       output.push(i);
-       output.push(i+1);
-       return output
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        console.log(i);
+        output.push(i);
+        output.push(j);
+        return output;
+      }
     }
   }
 }
 
-console.log(indices([4, 2, 3], 5));
+console.log(indices([3,2,4], 6));
